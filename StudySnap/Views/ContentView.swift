918d0d5e5cbca3639a0ee2bb364a0c0f63428e8a
fendylo <fendy.lomanjaya123@gmail.com>
2025-04-21 16:41:09 +1000

    Organise folder structure and Groq AI API sample call

---
+++ StudySnap/Views/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  StudySnap
+//
+//  Created by Fendy Lomanjaya on 21/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
