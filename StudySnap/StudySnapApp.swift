6d936d50406b3b350ee0f7e8e1f1dabf61b580ae
fendylo <fendy.lomanjaya123@gmail.com>
2025-04-21 13:53:45 +1000

    Initial Commit

---
+++ StudySnap/StudySnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySnapApp.swift
+//  StudySnap
+//
+//  Created by Fendy Lomanjaya on 21/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct StudySnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
